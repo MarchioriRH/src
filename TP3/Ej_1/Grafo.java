0a376f454717894a58420a827d694d7008849a88
MarchioriRH <rubenmarchiori@gmail.com>
2023-04-26 18:32:56 -0300

    Prueba

---
+++ TP3/Ej_1/Grafo.java
@@ -0,0 +1,17 @@
+package TP3.Ej_1;
+
+import java.util.ArrayList;
+
+public class Grafo {
+    private Integer vertice;
+    private ArrayList<Integer> archs;
+
+    public void grafo(Integer v){
+        this.archs = new ArrayList<>();
+        this.vertice = v;
+    }
+
+    public void addVertice(){
+        
+    }
+}
